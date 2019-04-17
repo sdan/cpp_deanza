db0882b27ebe778b20b3cb231dd1784aa0b9851d
Surya Dantuluri <surya.mvhs@gmail.com>
2019-04-17 11:04:38 -0700

    init

---
+++ AssA.cpp
@@ -0,0 +1,24 @@
+/*
+Surya Dantuluri
+CIS 22B
+Spring 2019
+Assignment A
+Problem number such as: Problem A1
+Description of problem:
+a few lines describing input, activity, and output of the program
+*/
+#import <iostream>
+#import <string>
+using namespace std;
+void getSize(int&);
+void getSpace(int, string*);
+void inputData(int, string*);
+void printData(string* , int);
+void destroy(string*);
+
+int main()
+{
+	int size = 0;
+	string *arr = null;
+
+}
